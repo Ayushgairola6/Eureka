dba9daee7faf88c57b236753a4bae94ba2a3f977
Ayush <Ayushgairola2002@gmail.com>
2025-09-10 20:53:12 +0530

    trying to fix the redis issue

--- backend/CachingHandler/redisClient.js
+++ backend/CachingHandler/redisClient.js
@@ -2,8 +2,6 @@ import { createClient } from "redis";
 import dotenv from "dotenv";
 dotenv.config();
 
-import { createClient } from "redis";
-
 const redisClient = createClient({
   url: process.env.REDIS_STRING,
 });
