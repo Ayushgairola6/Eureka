1cbe1d542bf58e8217f17e64c82dbc9c50ce1b58
Ayush gairola <ayushgairola2002@gmail.com>
2026-01-05 01:04:56 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -24,7 +24,7 @@ app.use(
       "http://localhost:5173",
       "https://antinode-six-eta.vercel.app",
       "https://eureka-six-eta.vercel.app",
-      'https://antinodeai.space"
+      "https://antinodeai.space"
     ],
     credentials: true,
     allowedHeaders: [
